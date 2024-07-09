jdomnitz/net-mdns

commit 4fd0cfc81f8cad49706722fbf2ec9ba5de3aa39c

Set the cache-flush bit when we own the service records

src/MulticastService.cs
@@ -36,6 +36,16 @@ namespace Makaretu.Dns
         private int maxPacketSize;
 
         /// <summary>
+        /// When this bit is set in a question, it indicates that the querier is willing to accept unicast replies in response to this specific query,
+        /// as well as the usual multicast responses.
+        /// </summary>
+        public const int UNICAST_RESPONSE_BIT = 0x8000;
+        /// <summary>
+        /// If the record is one that has been verified unique, the host sets the most significant bit of the rrclass field of the resource record.
+        /// This bit, the cache-flush bit, tells neighboring hosts that this is not a shared record type.
+        /// </summary>
+        public const int CACHE_FLUSH_BIT = 0x8000;
+        /// <summary>
         ///   Recently sent messages.
         /// </summary>
         private readonly RecentMessages sentMessages = new RecentMessages();
@@ -435,7 +445,7 @@ namespace Makaretu.Dns
             msg.Questions.Add(new Question
             {
                 Name = name,
-                Class = (DnsClass)((ushort)@class | 0x8000),
+                Class = (DnsClass)((ushort)@class | UNICAST_RESPONSE_BIT),
                 Type = type
             });
 

src/ServiceDiscovery.cs
@@ -269,7 +269,7 @@ namespace Makaretu.Dns
             {
                 if (e.Message.Answers.Count > 0)
                 {
-                    if (e.Message.Answers[0].Class == DnsClass.IN && e.Message.Answers[0].Name.Equals(profile.HostName))
+                    if ((DnsClass)((ushort)e.Message.Answers[0].Class & ~MulticastService.CACHE_FLUSH_BIT) == DnsClass.IN && e.Message.Answers[0].Name.Equals(profile.HostName))
                         conflict = true;
                 }
             };
@@ -313,11 +313,13 @@ namespace Makaretu.Dns
 
             // Add the shared records.
             var ptrRecord = new PTRRecord { Name = profile.QualifiedServiceName, DomainName = profile.FullyQualifiedName };
+            ptrRecord.Class = (DnsClass)((ushort)ptrRecord.Class | MulticastService.CACHE_FLUSH_BIT);
             message.Answers.Add(ptrRecord);
 
             // Add the resource records.
             profile.Resources.ForEach((resource) =>
             {
+                resource.Class = (DnsClass)((ushort)ptrRecord.Class | MulticastService.CACHE_FLUSH_BIT);
                 message.Answers.Add(resource);
             });
 
@@ -420,10 +422,10 @@ namespace Makaretu.Dns
             var QU = false; // unicast query response?
             foreach (var r in request.Questions)
             {
-                if (((ushort)r.Class & 0x8000) != 0)
+                if (((ushort)r.Class & MulticastService.UNICAST_RESPONSE_BIT) != 0)
                 {
                     QU = true;
-                    r.Class = (DnsClass)((ushort)r.Class & 0x7fff);
+                    r.Class = (DnsClass)((ushort)r.Class & ~MulticastService.UNICAST_RESPONSE_BIT);
                 }
             }
 

test/MulticastServiceTest.cs
@@ -569,7 +569,6 @@ namespace Makaretu.Dns
         }
 
         [TestMethod]
-        [Ignore("#52")]
         public async Task NoDuplicateResponse()
         {
             var service = Guid.NewGuid().ToString() + ".local";

commit c5ad0448ca0c6832cef47d13289482c2f811d3fe

Fix Announce regression introduced in 4fd0cfc (v0.33) Handle OS without dual stack network Correct duplicate age out documentation

src/Mdns.csproj
@@ -9,8 +9,8 @@
     <DebugType>full</DebugType>
     <DisableImplicitNuGetFallbackFolder>true</DisableImplicitNuGetFallbackFolder>
     
-    <AssemblyVersion>0.34</AssemblyVersion>
-    <Version>0.34</Version>
+    <AssemblyVersion>0.35</AssemblyVersion>
+    <Version>0.35</Version>
     
     <!-- Nuget specs -->
     <PackageId>Makaretu.Dns.Multicast.New</PackageId>

src/MulticastService.cs
@@ -63,12 +63,12 @@ namespace Makaretu.Dns
         /// <summary>
         ///   Use to send unicast IPv4 answers.
         /// </summary>
-        private readonly UdpClient unicastClientIp4 = new UdpClient(AddressFamily.InterNetwork);
+        private readonly UdpClient? unicastClientIp4;
 
         /// <summary>
         ///   Use to send unicast IPv6 answers.
         /// </summary>
-        private readonly UdpClient unicastClientIp6 = new UdpClient(AddressFamily.InterNetworkV6);
+        private readonly UdpClient? unicastClientIp6;
 
         /// <summary>
         ///   Function used for listening filtered network interfaces.
@@ -139,7 +139,11 @@ namespace Makaretu.Dns
             networkInterfacesFilter = filter;
 
             UseIpv4 = Socket.OSSupportsIPv4;
+            if (UseIpv4)
+                unicastClientIp4 = new UdpClient(AddressFamily.InterNetwork);
             UseIpv6 = Socket.OSSupportsIPv6;
+            if (UseIpv6)
+                unicastClientIp6 = new UdpClient(AddressFamily.InterNetworkV6);
             IgnoreDuplicateMessages = true;
         }
 
@@ -166,7 +170,7 @@ namespace Makaretu.Dns
         ///   <b>true</b> to ignore duplicate messages. Defaults to <b>true</b>.
         /// </value>
         /// <remarks>
-        ///   When set, a message that has been received within the last minute
+        ///   When set, a message that has been received within the last second
         ///   will be ignored.
         /// </remarks>
         public bool IgnoreDuplicateMessages { get; set; }
@@ -621,9 +625,9 @@ namespace Makaretu.Dns
                 return;
             }
 
-            // Standard multicast reponse?
             if (remoteEndPoint == null)
             {
+                // Standard multicast reponse
                 client?.SendAsync(packet).GetAwaiter().GetResult();
             }
             // Unicast response
@@ -631,7 +635,8 @@ namespace Makaretu.Dns
             {
                 var unicastClient = (remoteEndPoint.Address.AddressFamily == AddressFamily.InterNetwork)
                     ? unicastClientIp4 : unicastClientIp6;
-                unicastClient.SendAsync(packet, packet.Length, remoteEndPoint).GetAwaiter().GetResult();
+                if (unicastClient != null) //If the OS doesn't support the address family we can't send
+                    unicastClient.SendAsync(packet, packet.Length, remoteEndPoint).GetAwaiter().GetResult();
             }
         }
 

src/ServiceDiscovery.cs
@@ -322,13 +322,14 @@ namespace Makaretu.Dns
             // Add the resource records.
             profile.Resources.ForEach((resource) =>
             {
-                resource.Class = (DnsClass)((ushort)ptrRecord.Class | MulticastService.CACHE_FLUSH_BIT);
-                message.Answers.Add(resource);
+                var newResource = resource.Clone() as ResourceRecord;
+                newResource.Class = (DnsClass)((ushort)newResource.Class | MulticastService.CACHE_FLUSH_BIT);
+                message.Answers.Add(newResource);
             });
 
-            Mdns.SendAnswer(message, checkDuplicate: false);
+            Mdns.SendAnswer(message, false);
             Task.Delay(1000).Wait();
-            Mdns.SendAnswer(message, checkDuplicate: false);
+            Mdns.SendAnswer(message, false);
         }
 
         /// <summary>